704754e2200bfa5bab707157790157211b816f27
Ddgodoy16 <elvinnu_es25@outlook.es>
2024-02-11 21:32:33 -0600

    codigo completo

---
+++ js/index.js
@@ -0,0 +1,15 @@
+fetch("https://api.escuelajs.co/api/v1/categories")
+    .then(res => res.json())
+    .then(data => {
+        let card = '';
+        for (let db of data) {
+            card += `<div class="card mb-5" >
+                    <img src="${db.image}" class="card-img-top" alt="${db.name}">
+                
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><strong>Nombre:</strong> ${db.name}</li>
+                        </ul>
+                </div>`
+        }
+        document.getElementById("lista").innerHTML = card
+    })
